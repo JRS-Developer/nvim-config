65fa08b7a159f55c728ee64f6a02c6610667d036
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-01-31 13:02:55 -0400

    Cambiado algunas configuraciones

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -14,11 +14,11 @@ vim.o.smartindent = true
 vim.o.expandtab = true
 
 -- Scroll
-vim.o.scrolloff = 8
+vim.o.scrolloff = 8 -- Scroll off
 
 -- Searching
-vim.o.incsearch = true
-vim.o.hlsearch = false
+vim.o.incsearch = true -- Searching
+vim.o.hlsearch = true -- Highlight search
 
 -- Cursor
 vim.o.cursorline = true
@@ -36,6 +36,13 @@ vim.o.foldmethod = "manual"
 vim.o.foldcolumn = "auto"
 
 vim.opt.timeoutlen = 100 -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.undofile = true -- enable undo file
+vim.opt.wrap = false -- disable line wrapping
+vim.opt.showmode = false -- show mode
+vim.opt.updatetime = 300 -- time to wait for a redraw (in milliseconds)
+vim.opt.backup = false -- creates a backup file
+vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.swapfile = false -- creates a swapfile
 
 vim.cmd("autocmd BufWinLeave *.* silent! mkview!") -- Create a view for each file
 vim.cmd("autocmd BufWinEnter *.* silent! loadview") -- Load the view for each file
