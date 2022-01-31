3261311b371fb3944e169783c9b081e452a9de10
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-01-31 10:14:32 -0400

    Configurado colorizer para que muestre color en todos los modos.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -42,9 +42,7 @@ return packer.startup({
 
 		use({
 			"norcalli/nvim-colorizer.lua",
-			config = function()
-				require("colorizer").setup()
-			end,
+			config = "require('colorizer-config')",
 		})
 
 		use({
