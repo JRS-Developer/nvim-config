e86f843bfbfe2800e47b2f3ca7f332642fc110b1
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-02-07 12:21:13 -0400

    Eliminado codigo comentado y agregado un icono al crear breakpoints

--- lua/dap-config/init.lua
+++ lua/dap-config/init.lua
@@ -1,5 +1,3 @@
--- local dap = require("dap")
---
 local dap_install = require("dap-install")
 
 dap_install.setup({
@@ -13,80 +11,5 @@ for _, debugger in ipairs(dbg_list) do
 	dap_install.config(debugger)
 end
 
--- dap.adapters.chrome = {
--- 	type = "executable",
--- 	command = "node",
--- 	args = { install_path .. "vscode-chrome-debug\\out\\src\\chromeDebug.js" }, -- TODO adjust
--- }
---
--- dap.adapters.node2 = {
--- 	type = "executable",
--- 	command = "node",
--- 	args = { install_path .. "vscode-node-debug2\\out\\src\\nodeDebug.js" },
--- }
---
--- dap.configurations.javascript = { -- change this to javascript if needed
--- 	{
--- 		name = "Launch",
--- 		type = "node2",
--- 		request = "launch",
--- 		program = "${file}",
--- 		cwd = vim.fn.getcwd(),
--- 		sourceMaps = true,
--- 		protocol = "inspector",
--- 		console = "integratedTerminal",
--- 	},
--- 	{
--- 		-- For this to work you need to make sure the node process is started with the `--inspect` flag.
--- 		name = "Attach to process",
--- 		type = "node2",
--- 		request = "attach",
--- 		processId = require("dap.utils").pick_process,
--- 	},
--- }
---
--- dap.configurations.typescript = { -- change this to javascript if needed
--- 	{
--- 		name = "Launch",
--- 		type = "node2",
--- 		request = "launch",
--- 		program = "${file}",
--- 		cwd = vim.fn.getcwd(),
--- 		sourceMaps = true,
--- 		protocol = "inspector",
--- 		console = "integratedTerminal",
--- 	},
--- 	{
--- 		-- For this to work you need to make sure the node process is started with the `--inspect` flag.
--- 		name = "Attach to process",
--- 		type = "node2",
--- 		request = "attach",
--- 		processId = require("dap.utils").pick_process,
--- 	},
--- }
---
--- dap.configurations.javascriptreact = { -- change this to javascript if needed
--- 	{
--- 		type = "chrome",
--- 		request = "attach",
--- 		program = "${file}",
--- 		cwd = vim.fn.getcwd(),
--- 		sourceMaps = true,
--- 		protocol = "inspector",
--- 		port = 9222,
--- 		webRoot = "${workspaceFolder}",
--- 	},
--- }
---
--- dap.configurations.typescriptreact = { -- change to typescript if needed
--- 	{
--- 		type = "chrome",
--- 		request = "attach",
--- 		program = "${file}",
--- 		cwd = vim.fn.getcwd(),
--- 		sourceMaps = true,
--- 		protocol = "inspector",
--- 		port = 9222,
--- 		webRoot = "${workspaceFolder}",
--- 	},
--- }
+-- Icons
+vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
