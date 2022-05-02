8c3466e901fd09935add1886f3b2923f7270ca32
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-05-02 10:08:07 -0400

    vim: commented dap config up to plugin get fixed

--- lua/dap-config/init.lua
+++ lua/dap-config/init.lua
@@ -1,15 +1,15 @@
 local dap_install = require("dap-buddy")
 
-dap_install.setup({
-	installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
-})
+--dap_install.setup({
+--	installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
+--})
 
 -- Configure each dap server already installed
-local dbg_list = require("dap-install.api.debuggers").get_installed_debuggers()
+-- local dbg_list = require("dap-install.api.debuggers").get_installed_debuggers()
 
-for _, debugger in ipairs(dbg_list) do
-	dap_install.config(debugger)
-end
+--for _, debugger in ipairs(dbg_list) do
+--	dap_install.config(debugger)
+--end
 
 -- Icons
-vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
+--vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "", linehl = "", numhl = "" })
