9975400bcb62ffe113aada3272e5a935ef8353ea
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-05-26 09:05:45 -0400

    feat: updated catppuccin theme config

--- lua/catppuccin-config/init.lua
+++ lua/catppuccin-config/init.lua
@@ -1,41 +1,22 @@
 local catppuccin = require("catppuccin")
 
--- Catppuccin color reference:
-local color_palette = {
-	rosewater = "#F5E0DC", -- Rosewater
-	flamingo = "#F2CDCD", -- Flamingo
-	mauve = "#DDB6F2", -- Mauve
-	pink = "#F5C2E7", -- Pink
-	red = "#F28FAD", -- Red
-	maroon = "#E8A2AF", -- Maroon
-	peach = "#F8BD96", -- Peach
-	yellow = "#FAE3B0", -- Yellow
-	green = "#ABE9B3", -- Green
-	blue = "#96CDFB", -- Blue
-	sky = "#89DCEB", -- Sky
-	teal = "#B5E8E0", -- Teal
-	lavender = "#C9CBFF", -- Lavender
-	white = "#D9E0EE", -- White
-	gray2 = "#C3BAC6", -- Gray2
-	gray1 = "#988BA2", -- Gray1
-	gray0 = "#6E6C7E", -- Gray0
-	black4 = "#575268", -- Black4
-	black3 = "#302D41", -- Black3
-	black2 = "#1E1E2E", -- Black2
-	black1 = "#1A1826", -- Black1
-	black0 = "#161320", -- Black0
-}
-
 -- configure it
 catppuccin.setup({
 	transparent_background = false,
 	term_colors = true,
 	styles = {
 		comments = "italic",
-		functions = "italic",
-		keywords = "italic",
+		conditionals = "italic",
+		loops = "NONE",
+		functions = "NONE",
+		keywords = "NONE",
 		strings = "NONE",
-		variables = "italic",
+		variables = "NONE",
+		numbers = "NONE",
+		booleans = "NONE",
+		properties = "NONE",
+		types = "NONE",
+		operators = "NONE",
 	},
 	integrations = {
 		treesitter = true,
@@ -63,9 +44,14 @@ catppuccin.setup({
 		nvimtree = {
 			enabled = true,
 			show_root = false,
-			transparent_panel = false, -- make the panel transparent
+			transparent_panel = false,
 		},
-		which_key = false,
+		neotree = {
+			enabled = false,
+			show_root = false,
+			transparent_panel = false,
+		},
+		which_key = true,
 		indent_blankline = {
 			enabled = true,
 			colored_indent_levels = true,
@@ -80,9 +66,11 @@ catppuccin.setup({
 		lightspeed = false,
 		ts_rainbow = true,
 		hop = false,
-		notify = true,
-		telekasten = true,
+		notify = false,
+		telekasten = false,
+		symbols_outline = false,
 	},
 })
 
+vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
 vim.cmd([[colorscheme catppuccin]])
