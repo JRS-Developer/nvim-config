c6f13166d34c054a2526cf4c8c8c8f6ecb0f2d87
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-06-26 18:30:20 -0400

    feat: changed the separators to be circled

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -2,8 +2,9 @@ require("lualine").setup({
 	options = {
 		icons_enabled = true,
 		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		-- component_separators = { left = "", right = "" },
+		component_separators = "",
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			"dashboard",
 			"NvimTree",
@@ -15,7 +16,7 @@ require("lualine").setup({
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
 		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_x = { "fileformat", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
 	},
