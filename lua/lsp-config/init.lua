12181b5037247c9e33b32c9150159dac5d384dde
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-08-25 14:48:54 -0400

    feat: moved config from lsp-config to lspsaga

--- lua/lsp-config/init.lua
+++ lua/lsp-config/init.lua
@@ -1,55 +1,9 @@
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-	local function buf_set_keymap(...)
-		vim.api.nvim_buf_set_keymap(bufnr, ...)
-	end
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
-	end
-
+local on_attach = function(client)
 	-- Vim Illuminate
 	require("illuminate").on_attach(client)
 	vim.g.Illuminate_ftblacklist = { "NvimTree" }
-
-	-- Enable completion triggered by <c-x><c-o>
-	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
-	-- Mappings.
-	local opts = { noremap = true, silent = true }
-
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-	buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-	buf_set_keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-	buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-
-	-- slp saga
-	buf_set_keymap("n", "<leader>ca", ":Lspsaga code_action<CR>", { silent = true, noremap = true })
-	buf_set_keymap("n", "gh", "<cmd>Lspsaga lsp_finder<cr>", { silent = true, noremap = true })
-
-	buf_set_keymap("n", "gr", "<cmd>Lspsaga rename<cr>", { silent = true, noremap = true })
-
-	buf_set_keymap("n", "<silent><leader>ca", "<cmd>Lspsaga code_action<cr>", { silent = true, noremap = true })
-
-	buf_set_keymap("n", "K", "<cmd>Lspsaga hover_doc<cr>", { silent = true, noremap = true })
-
-	buf_set_keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_next<cr>", { silent = true, noremap = true })
-	buf_set_keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<cr>", { silent = true, noremap = true })
-
-	buf_set_keymap(
-		"n",
-		"<C-u>",
-		"<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>",
-		{ noremap = true }
-	)
-	buf_set_keymap("n", "<C-d>", "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>", { noremap = true })
 
 	vim.diagnostic.config({
 		virtual_text = {
