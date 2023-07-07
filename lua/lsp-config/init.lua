26abebe3fb648507ebfdede8cfde2b28cf3013cd
Jose Sanchez <sanchezjoserafael25@gmail.com>
2023-07-07 11:16:10 -0400

    feat: changed the icon position of cmp completion

--- lua/lsp-config/init.lua
+++ lua/lsp-config/init.lua
@@ -71,14 +71,31 @@ cmp.setup({
 	},
 	window = {
 		completion = cmp.config.window.bordered(),
+
 		documentation = cmp.config.window.bordered(),
 	},
+
+	-- formatting = {
+	-- 	format = lspkind.cmp_format({
+	-- 		mode = "symbol", -- show only symbol annotations
+	-- 		maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+	-- 		symbol_map = { Copilot = "" }, -- Add copilot icon
+	-- 	}),
+	-- },
 	formatting = {
-		format = lspkind.cmp_format({
-			mode = "symbol", -- show only symbol annotations
-			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-			symbol_map = { Copilot = "" }, -- Add copilot icon
-		}),
+		fields = { "kind", "abbr", "menu" },
+		format = function(entry, vim_item)
+			local kind = lspkind.cmp_format({
+				mode = "symbol_text",
+				maxwidth = 50,
+				symbol_map = { Copilot = "" },
+			})(entry, vim_item)
+			local strings = vim.split(kind.kind, "%s", { trimempty = true })
+			kind.kind = " " .. (strings[1] or "") .. " "
+			kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+			return kind
+		end,
 	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -135,7 +152,46 @@ cmp.setup({
 		},
 	},
 })
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#282C34", fg = "NONE" })
+vim.api.nvim_set_hl(0, "Pmenu", { fg = "#C5CDD9", bg = "#22252A" })
 
+vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
+
+vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#EED8DA" })
+vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#EED8DA" })
+vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#EED8DA" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#C3E88D" })
+vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#C3E88D" })
+vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#C3E88D" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#FFE082" })
+vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#FFE082" })
+vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#FFE082" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#EADFF0" })
+vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#EADFF0" })
+vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#EADFF0" })
+vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#EADFF0" })
+vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#EADFF0" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#C5CDD9" })
+vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#C5CDD9" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#F5EBD9" })
+vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#F5EBD9" })
+vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#F5EBD9" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = "#DDE5F5" })
+vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = "#DDE5F5" })
+vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = "#DDE5F5" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#D8EEEB" })
+vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = "#D8EEEB" })
+vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = "#D8EEEB" })
 -- Set configuration for specific filetype.
 cmp.setup.filetype("gitcommit", {
 	sources = cmp.config.sources({
