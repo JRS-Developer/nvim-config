366a28e5d47da950b60b69a21157566e44f5dd04
Jose Sanchez <sanchezjoserafael25@gmail.com>
2023-07-07 11:12:16 -0400

    added flash nvim for motions

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -37,6 +37,61 @@ lazy.setup({
 		end,
 	},
 
+	-- Motion
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "o" },
+				function()
+					require("flash").jump({
+						search = { -- Match beginning of words only
+							mode = function(str)
+								return "\\<" .. str
+							end,
+						},
+					})
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "o", "x" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Flash Treesitter Search",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
+	},
+
 	{
 
 		"lukas-reineke/indent-blankline.nvim",
