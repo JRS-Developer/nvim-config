8b8a60ebc624cb2407a8eaa1016c372c666d24ec
Jose Sanchez <sanchezjoserafael25@gmail.com>
2024-12-09 16:05:03 -0400

    feat: removed import cost due to high cpu usage

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -210,13 +210,13 @@ lazy.setup({
 		},
 	},
 
-	{
-		"barrett-ruth/import-cost.nvim",
-		build = "sh install.sh yarn",
-		-- if on windows
-		-- build = 'pwsh install.ps1 yarn',
-		config = true,
-	},
+	-- {
+	-- 	"barrett-ruth/import-cost.nvim", -- High CPU usage
+	-- 	build = "sh install.sh yarn",
+	-- 	-- if on windows
+	-- 	-- build = 'pwsh install.ps1 yarn',
+	-- 	config = true,
+	-- },
 
 	-- LSP
 	{
