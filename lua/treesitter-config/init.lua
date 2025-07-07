8bb6e2807a9bcf9b19497ee27abbb2e04c70110c
Jose Sanchez <sanchezjoserafael25@gmail.com>
2025-07-07 11:08:26 -0400

    fix: ipkg gone

--- lua/treesitter-config/init.lua
+++ lua/treesitter-config/init.lua
@@ -1,6 +1,8 @@
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "all",
 
+	ignore_install = { "ipkg" },
+
 	highlight = {
 		-- `false` will disable the whole extension
 		enable = true,
